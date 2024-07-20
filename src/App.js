895748ea62af36e21c104a995033f17ae12fe587
IsraelMg27 <angelmontoya1130@gmail.com>
2024-07-19 22:03:57 -0600

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+import Inicio from './components/Inicio';
+import Usuarios from './components/Usuarios';
+import Register from './components/Register';
+import ReCon from './components/ReCon';
+import ResetPassword from './components/ResetPassword'; // Nuevo componente importado
+import './styles/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/usuarios" element={<Usuarios />} />
+          <Route path="/recon" element={<ReCon />} />
+          <Route path="/reset/:token" element={<ResetPassword />} /> {/* Nueva ruta añadida */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
